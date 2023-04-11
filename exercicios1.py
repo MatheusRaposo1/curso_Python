5b5c540932303f3455cf326817305ad17f86fa02
MatheusRaposo1 <matheus-raposo1@hotmail.com>
2023-04-11 12:06:27 -0300

    finalizado teste 4

---
+++ exercicios1.py
@@ -0,0 +1,13 @@
+nome = "Matheus"
+sobrenome = "Raposo"
+idade = 23
+ano_nascimento = 1999
+maior_de_idade = idade >= 18
+altura_metros = 1.75
+
+
+print("Nome:", nome, sobrenome, type(nome), type(sobrenome))
+print("Idade:", idade , type(idade))
+print("Ano de nascimento:", ano_nascimento, type(ano_nascimento))
+print("Maior de idade:", maior_de_idade, type(maior_de_idade))
+print("Altura:", altura_metros, type(altura_metros))
---
+++ exercicios2.py
@@ -0,0 +1,8 @@
+nome = "Matheus"
+altura = 1.74
+peso = 90
+imc = peso / altura ** 2  #(...) -> Ellipsis
+
+print(nome, "Tem", altura, "de altura")
+print("Pesa", peso, "e seu IMC é:", imc)
+
