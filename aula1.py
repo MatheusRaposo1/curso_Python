9af2f562a683ac0cfcf631dd7d1ac64ea5d64172
MatheusRaposo1 <matheus-raposo1@hotmail.com>
2023-04-11 10:41:46 -0300

    Aula python

---
+++ aula1.py
@@ -0,0 +1,13 @@
+"""
+DocString
+ E escrever o que eu quiser
+"""
+'''
+ Usar para escrever suas notas 
+'''
+
+
+# Permite escrever um comentário.
+print(123) #Na frente.
+# Abaixo
+print("Hello , world")
---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Matheus Raposo')
+print(1, 'Matheus "Raposo"')
+
+# Aspas duplas
+print("Matheus Raposo")
+print(2, "Matheus 'Raposo'")
+
+# Escape
+print("Matheus \"Raposo")
+
+# r
+print(r"Matheus \"Raposo\"")
