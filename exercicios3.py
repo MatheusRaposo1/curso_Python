01242a7a0f6ff5237a1405a8251c8243b71c1f75
MatheusRaposo1 <matheus-raposo1@hotmail.com>
2023-04-12 11:08:02 -0300

    Aulas de python até o numero 19

---
+++ exercicios3.py
@@ -0,0 +1,14 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'O {primeiro_valor} é maior  ou igual '
+        f' ao que {segundo_valor}'  
+          )
+
+elif segundo_valor >= primeiro_valor:
+     print(
+        f'O {segundo_valor} é maior que ou igual '
+        f'ao que {primeiro_valor}'  
+          )     
